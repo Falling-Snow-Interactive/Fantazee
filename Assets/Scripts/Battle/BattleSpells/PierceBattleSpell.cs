0608474b029b0053fa6b1fffefa3a58de95ca950
Kira Dinan <kiradinan.dev@gmail.com>
2025-03-05 13:54:27 -0400

    pierce vfx

--- Assets/Scripts/Battle/BattleSpells/PierceBattleSpell.cs
+++ Assets/Scripts/Battle/BattleSpells/PierceBattleSpell.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using Fantazee.Battle.Characters.Enemies;
 using Fantazee.Battle.Characters.Player;
 using Fantazee.Spells.Data;
+using FMOD.Studio;
+using FMODUnity;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Fantazee.Battle.BattleSpells
 {
@@ -13,9 +17,21 @@ namespace Fantazee.Battle.BattleSpells
     {
         private PierceData pierceData;
         
+        private EventInstance pierceLoopSfx;
+        private EventInstance pierceHitSfx;
+        
         public PierceBattleSpell(PierceData data) : base(data)
         {
             pierceData = data;
+            if (!pierceData.TweenSfx.IsNull)
+            {
+                pierceLoopSfx = RuntimeManager.CreateInstance(data.TweenSfx);
+            }
+
+            if (!pierceData.HitSfx.IsNull)
+            {
+                pierceHitSfx = RuntimeManager.CreateInstance(data.HitSfx);
+            }
         }
 
         protected override IEnumerator CastSequence(Damage damage, Action onComplete = null)
@@ -28,20 +44,59 @@ namespace Fantazee.Battle.BattleSpells
             yield return new WaitForSeconds(0.2f);
 
             BattleEnemy e0 = enemies[^1];
-            BattleEnemy e1 = null;
-            if (enemies.Count > 1)
+            if (e0)
             {
-                e1 = enemies[^2];
+                BattleEnemy e1 = null;
+                if (enemies.Count > 1)
+                {
+                    e1 = enemies[^2];
+                }
+
+                Vector3 startPos = player.transform.position + pierceData.TweenVfxSpawnOffset;
+                Vector3 endPos = e0.transform.position + pierceData.TweenVfxSpawnOffset;
+                if (e1 != null)
+                {
+                    endPos = e1.transform.position + pierceData.TweenVfxSpawnOffset;
+                }
+
+                bool ready = false;
+                var arrow = Object.Instantiate(pierceData.TweenVfx, startPos, Quaternion.identity);
+                arrow.transform.DOMove(endPos, pierceData.TweenTime)
+                     .SetDelay(pierceData.TweenDelay)
+                     .SetEase(pierceData.TweenEase)
+                     .OnComplete(() =>
+                                 {
+                                     Object.Destroy(arrow.gameObject);
+                                     
+                                     ready = true;
+                                 });
+
+                yield return new WaitUntil(() => ready);
+                
+                if (pierceHitSfx.isValid())
+                {
+                    pierceHitSfx.start();
+                }
+                e0.Damage(Mathf.RoundToInt(damage.Value * pierceData.FirstEnemyMod));
+                if (pierceData.HitVfx)
+                {
+                    Object.Instantiate(pierceData.HitVfx,
+                                                   e0.transform.position + pierceData.TweenVfxHitOffset,
+                                                   e0.transform.rotation);
+                }
+
+                if (e1)
+                {
+                    e1.Damage(Mathf.RoundToInt(damage.Value * pierceData.SecondEnemyMod));
+                    if (pierceData.HitVfx)
+                    {
+                        Object.Instantiate(pierceData.HitVfx,
+                                           e1.transform.position + pierceData.TweenVfxHitOffset,
+                                           e1.transform.rotation);
+                    }
+                }
             }
 
-            e0.Damage(Mathf.RoundToInt(damage.Value * pierceData.FirstEnemyMod));
-
-            if (e1)
-            {
-                yield return new WaitForSeconds(0.2f);
-                e1.Damage(Mathf.RoundToInt(damage.Value * pierceData.SecondEnemyMod));
-            }
-            
             yield return new WaitForSeconds(1f);
             
             onComplete?.Invoke();
--- Assets/Scripts/Spells/Data/FireballSpellData.cs
+++ Assets/Scripts/Spells/Data/FireballSpellData.cs
@@ -1,3 +1,5 @@
+using DG.Tweening;
+using FMODUnity;
 using UnityEngine;
 
 namespace Fantazee.Spells.Data
@@ -6,5 +8,47 @@ namespace Fantazee.Spells.Data
     public class FireballSpellData : SpellData
     {
         public override SpellType Type => SpellType.Fireball;
+        
+        [Header("Fireball")]
+        
+        [Header("Transit")]
+
+        [SerializeField]
+        private GameObject tweenVfx;
+        public GameObject TweenVfx => tweenVfx;
+        
+        [SerializeField]
+        private EventReference tweenSfx;
+        public EventReference TweenSfx => tweenSfx;
+
+        [SerializeField]
+        private Vector3 tweenVfxSpawnOffset;
+        public Vector3 TweenVfxSpawnOffset => tweenVfxSpawnOffset;
+        
+        [SerializeField]
+        private Vector3 tweenVfxHitOffset;
+        public Vector3 TweenVfxHitOffset => tweenVfxHitOffset;
+
+        [SerializeField]
+        private Ease tweenEase = Ease.Linear;
+        public Ease TweenEase => tweenEase;
+
+        [SerializeField]
+        private float tweenTime = 0.6f;
+        public float TweenTime => tweenTime;
+
+        [SerializeField]
+        private float tweenDelay = 0.35f;
+        public float TweenDelay => tweenDelay;
+        
+        [Header("Hit")]
+
+        [SerializeField]
+        private GameObject hitVfx;
+        public GameObject HitVfx => hitVfx;
+
+        [SerializeField]
+        private EventReference hitSfx;
+        public EventReference HitSfx => hitSfx;
     }
 }
