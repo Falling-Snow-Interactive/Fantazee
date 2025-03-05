add586180623ce57a7e5c61987242c3887db2a8b
Kira Dinan <kiradinan.dev@gmail.com>
2025-03-05 11:50:51 -0400

    Added to store

--- Assets/Scripts/Shop/Buckets/Spells/ShopSpellBucketEntry.cs
+++ Assets/Scripts/Shop/Buckets/Spells/ShopSpellBucketEntry.cs
@@ -2,12 +2,12 @@ using System;
 using Fantazee.Shop.Items;
 using Fantazee.Spells;
 using Fsi.Gameplay.Buckets;
+using UnityEngine;
 
 namespace Fantazee.Shop.Buckets.Spells
 {
     [Serializable]
     public class ShopSpellBucketEntry : BucketEntry<SpellShopItem>
     {
-        
     }
 }
