0889797b22a60ab7b141b98d4fbb5c854427370b
Kira Dinan <kiradinan.dev@gmail.com>
2025-03-26 10:35:59 -0300

    Okay getting the main menu working. Should probably change how the button is laid out

--- Assets/Scripts/Ui/Buttons/SimpleButton.cs
+++ Assets/Scripts/Ui/Buttons/SimpleButton.cs
@@ -3,102 +3,132 @@ using Fantazee.Scores.Settings;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-namespace Fantazee.Ui.Buttons;
-
-public class SimpleButton : MonoBehaviour
+namespace Fantazee.Ui.Buttons
 {
-    public event Action<SimpleButton> Selected;
-    public event Action<SimpleButton> Deselected;
-    
-    private bool isSelected;
-    private bool isDisabled;
-    
-    [Header("Simple Button")]
-    
-    [SerializeField]
-    private List<BackgroundRef> backgroundRefs = new();
-    
-    [SerializeField]
-    protected Button button;
 
-    protected virtual BackgroundColorPalette ColorPalette => ScoreSettings.Settings.ButtonColorPalette;
-    
-    private void OnValidate()
+    public class SimpleButton : MonoBehaviour
     {
-        foreach (BackgroundRef bg in backgroundRefs)
-        {
-            ColorPalette.NormalColors.Apply(bg);
-        }
-    }
-    
-    #region Ui Events
+        public event Action<SimpleButton> Selected;
+        public event Action<SimpleButton> Deselected;
 
-    public virtual void OnClick()
-    {
-        foreach (BackgroundRef bg in backgroundRefs)
-        {
-            ColorPalette.ClickedColors.InOut(bg);
-        }
-    }
+        private bool isSelected;
+        private bool isDisabled;
 
-    public void OnSelect(BaseEventData _ = null)
-    {
-        isSelected = true;
-        UpdateColors();
-        Selected?.Invoke(this);
-    }
+        [Header("Simple Button")]
 
-    public void OnDeselect(BaseEventData _ = null)
-    {
-        isSelected = false;
-        UpdateColors();
-        Deselected?.Invoke(this);
-    }
+        [SerializeField]
+        private List<BackgroundRef> backgroundRefs = new();
 
-    public void OnPointerEnter(PointerEventData _ = null)
-    {
-        EventSystem.current.SetSelectedGameObject(gameObject);
-    }
+        [SerializeField]
+        protected Button button;
 
-    private void UpdateColors()
-    {
-        foreach (BackgroundRef bg in backgroundRefs)
-        {
-            ColorPalette.NormalColors.Apply(bg);
-        }
-        
-        // Select will just be the outline and take piority
-        if (isSelected)
+        protected virtual BackgroundColorPalette ColorPalette => ScoreSettings.Settings.ButtonColorPalette;
+
+        private void OnValidate()
         {
             foreach (BackgroundRef bg in backgroundRefs)
             {
-                ColorPalette.SelectedColors.ApplyOutline(bg);
+                ColorPalette.NormalColors.Apply(bg);
             }
         }
-        
-        if (isDisabled)
+
+        #region Ui Events
+
+        public virtual void OnClick()
         {
             foreach (BackgroundRef bg in backgroundRefs)
             {
-                ColorPalette.DisabledColors.ApplyBackground(bg);
+                ColorPalette.ClickedColors.InOut(bg);
             }
         }
-    }
 
-    public void SetInteractable(bool set)
-    {
-        bool back = isSelected;
-        
-        button.interactable = set;
-        isDisabled = !set;
+        public void OnSelect()
+        {
+            isSelected = true;
+            UpdateColors();
+            Selected?.Invoke(this);
+        }
 
-        if (back)
+        public void OnSelect(BaseEventData _)
+        {
+            OnSelect();
+        }
+
+        public void OnDeselect()
+        {
+            isSelected = false;
+            UpdateColors();
+            Deselected?.Invoke(this);
+        }
+
+        public void OnDeselect(BaseEventData _)
+        {
+            OnDeselect();
+        }
+
+        public void OnPointerEnter()
         {
             EventSystem.current.SetSelectedGameObject(gameObject);
         }
-        
-        UpdateColors();
-    }
 
-    #endregion
+        public void OnPointerEnter(PointerEventData _)
+        {
+            OnPointerEnter();
+        }
+
+        public void OnPointerExit()
+        {
+            if (EventSystem.current.currentSelectedGameObject == gameObject)
+            {
+                EventSystem.current.SetSelectedGameObject(null);
+            }
+        }
+        
+        public void OnPointerExit(PointerEventData _)
+        {
+            OnPointerEnter();
+        }
+
+        private void UpdateColors()
+        {
+            foreach (BackgroundRef bg in backgroundRefs)
+            {
+                ColorPalette.NormalColors.Apply(bg);
+            }
+
+            // Select will just be the outline and take piority
+            if (isSelected)
+            {
+                foreach (BackgroundRef bg in backgroundRefs)
+                {
+                    ColorPalette.SelectedColors.ApplyOutline(bg);
+                }
+            }
+
+            if (isDisabled)
+            {
+                foreach (BackgroundRef bg in backgroundRefs)
+                {
+                    ColorPalette.DisabledColors.ApplyBackground(bg);
+                }
+            }
+        }
+
+        public void SetInteractable(bool set)
+        {
+            bool back = isSelected;
+
+            button.interactable = set;
+            isDisabled = !set;
+
+            if (back)
+            {
+                EventSystem.current.SetSelectedGameObject(gameObject);
+            }
+
+            UpdateColors();
+        }
+
+        #endregion
+    }
 }
